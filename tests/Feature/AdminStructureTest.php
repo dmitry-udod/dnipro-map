d1dd9dd8580a90701bf70c88aeef10f5c759f8c8
Dmitry Udod <udod.dmitry@gmail.com>
2018-04-26 00:06:12 +0300

    Structures

--- tests/Feature/AdminStructureTest.php
+++ tests/Feature/AdminStructureTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 /**
@@ -25,5 +27,20 @@ class AdminStructureTest extends TestCase
     {
         $this->actingAs($this->asAdmin())->get($this->route('/admin/structures'))->assertStatus(200);
         $this->actingAs($this->asSuperAdmin())->get($this->route('/admin/structures'))->assertStatus(200);
+    }
+
+    /** @test */
+    public function structure_add_photo()
+    {
+        Storage::fake('public');
+        $response = $this->actingAs($this->asAdmin())->post($this->route('/admin/structures/upload'), [
+            'photo' => UploadedFile::fake()->image('photo.jpg')
+        ]);
+        // dd();
+// dd($files = Storage::files('public'));
+        // dd($response->json())    ;
+        // Storage::disk('photos')->assertExists(public_path('uploads/structures/') . $response->json()['data'][0]['name']);
+        // Storage::disk('public')->assertExists(public_path('uploads/structures/photo.jpg'));
+        // $response->json()['data'][0]['name'])
     }
 }
