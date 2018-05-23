174ee2c43edce8c1404a8b984339c664639cecb5
Dmitry Udod <udod.dmitry@gmail.com>
2018-05-23 09:05:05 +0300

    Fixse

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta property="og:image" content="{{ asset('icons/flogo.png') }}"/>
+    <meta property="og:image" content="{{ asset('icons/logo.png') }}"/>
     <link rel="shortcut icon" href="{{ asset('icons/favicon.ico') }}" type="image/x-icon">
 
     <!-- CSRF Token -->
