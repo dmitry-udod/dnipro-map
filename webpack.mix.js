31302544b30f94b4269635c7b2525f535e6bad9a
Dmitry Udod <udod.dmitry@gmail.com>
2018-07-16 10:41:26 +0300

    Minify css

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,7 +10,12 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.options({
+    processCssUrls: false
+});
 
 mix.js('resources/assets/js/app.js', 'public/js').version();
 mix.js('resources/assets/js_front/app.js', 'public/js_front').version();
-mix.sass('resources/assets/sass/app.scss', 'public/css').version();
+mix.sass('resources/assets/sass/app.scss', 'public/css', {
+    outputStyle: mix.inProduction ? 'compressed' : 'expanded'
+}).version();
